eeaa72a91ece6e73432e2226582308f36cc122a9
Chelsea <Chelsea@DESKTOP-ERKIH32>
2018-03-28 10:57:54 +0530

    getting refresh token

---
+++ App14/App14.Android/PushNotifications/FirebaseServiceId.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Firebase.Iid;
+
+namespace App14.Droid.PushNotifications
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.INSTANCE_ID_EVENT" })]
+    public class FirebaseServiceId : FirebaseInstanceIdService
+    {
+        const string TAG = "MyFirebaseIIDService";
+        public override void OnTokenRefresh()
+        {
+            var refreshedToken = FirebaseInstanceId.Instance.Token;
+            Log.Debug(TAG, "Refreshed token: " + refreshedToken);
+            //Settings.SSA_IosDeviceToken = refreshedToken;
+           // Settings.SSA_IosDeviceToken = refreshedToken;
+        }
+    }
+}
---
+++ App14/App14.Android/PushNotifications/MyFirebaseMessagingService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+
+namespace App14.Droid.PushNotifications
+{
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            try
+            {
+                string text = String.Empty;
+                base.OnMessageReceived(message);
+                Random random = new Random();
+
+                foreach (var item in message.Data)
+                {
+                    if (item.Key == "Message")
+                    {
+                        text = item.Value;
+                    }
+                }
+
+                var intent = new Intent(this, typeof(MainActivity));
+                intent.AddFlags(ActivityFlags.ClearTop | ActivityFlags.SingleTop | ActivityFlags.NewTask);
+
+                PendingIntent pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.UpdateCurrent | PendingIntentFlags.OneShot);
+
+                var defaultSoundUri = RingtoneManager.GetDefaultUri(RingtoneType.Notification);
+                var notificationBuilder = new NotificationCompat.Builder(this)
+                    .SetSmallIcon(Resource.Drawable.icon)
+                    .SetContentTitle("SSA")
+                    .SetContentText(text)
+                    .SetAutoCancel(true)
+                    .SetSound(defaultSoundUri)
+                    .SetContentIntent(pendingIntent);
+                var notificationManager = (NotificationManager)GetSystemService(Context.NotificationService);
+                notificationManager.Notify(random.Next(100000), notificationBuilder.Build());
+            }
+            catch (Exception e)
+            {
+            }
+        }
+    }
+}
